0fde5420c31783ace29885afe5145be4f61a1c31
Rich Leo <hello@richleo.me>
2025-10-28 21:13:17 +0000

    save private listing type build

--- dist/property.d.ts
+++ dist/property.d.ts
@@ -29,7 +29,7 @@ export interface Property {
     historicMarketPulse: MarketPulse[] | null;
     address: Address | null;
     slug?: string;
-    isQuickListing?: boolean;
+    isPrivateListing?: boolean;
 }
 export interface MarketGrade {
     grade: "A" | "B" | "C" | "D" | "F";
